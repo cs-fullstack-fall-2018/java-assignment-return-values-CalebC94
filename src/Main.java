6c402d0de7a06cca8ac8b45b9df71781ae855ed1
Caleb Canada <calebcanada13@gmail.com>
2018-07-24 10:26:13 -0500

    intial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int number =33;
+        int number1 = 45;
+        System.out.println(("The largest number is:" + largestNumber(number, number1)));
+    }
+    public static
+}
